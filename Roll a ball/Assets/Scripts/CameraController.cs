0278f7e16c740f85554a603791f2697f08628206
DESKTOP-1MR3A00\Catbrainz <sara.alexia.b@gmail.com>
2018-08-08 17:43:22 -0400

    no message

---
+++ Roll a ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;		
+	}
+
+	// LateUpdate is called once per frame after everything is done in Update
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+		
+	}
+}
